a507361b554d1598088aea154e585b096d7f05c6
whitefang-code <lucianoarn5@gmail.com>
2022-12-24 15:34:29 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author whitefang
+ *
+ */
+module chess_system {
+}
