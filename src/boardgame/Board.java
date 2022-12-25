cc7ace3aaba32106d9e392f7bc0ff49651074e37
whitefang-code <lucianoarn5@gmail.com>
2022-12-25 15:32:30 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,4 +36,9 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
+	public void placePiece(final Piece piece, final Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+
 }
